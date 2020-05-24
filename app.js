367b1342df5041e8bf323eb3d99757327cdc1866
Megumi <megumi67@gmail.com>
2020-05-24 13:14:00 +0900

    レビューいただいた箇所の修正

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ const questionNumber = document.getElementById('questionNumber')
 const catDifPlace = document.getElementById('catDif')
 const questionPlace = document.getElementById('question')
 const startBtn = document.getElementById('startBtn')
-const btnSection = document.querySelector('ul')
+const choiceBtnSection = document.querySelector('ul')
 
 
 //問題のインデックス番号とスコアを格納
 let currentNum = 0; 
 let score = 0;
-let QuestionNum = 1;
 
 //開始ボタンを押すとJSONデータを取得しクイズ画面描画関数に渡す
 startBtn.addEventListener('click', () => {
@@ -22,15 +21,15 @@ startBtn.addEventListener('click', () => {
     question.textContent = '少々お待ちください'
 
     fetch(url)
-               .then((response) => {
-                    return response.json()　//ここでBodyからJsonを返す
-                })
-                    .then((result) => {
-                                        setQuiz(result);                                        
-                                        })
-                    .catch((e)=>{
-                        console.log(e)
-                 })                    
+        .then((response) => {
+            return response.json()　//ここでBodyからJsonを返す
+        })
+            .then((result) => {
+                setQuiz(result);                                        
+            })
+            .catch((e)=>{
+                console.log(e)
+            })                    
 })            
 
 //正解の選択肢をシャッフルする関数
@@ -44,11 +43,11 @@ function shuffle(arr) {
 
 //クイズ画面を作成する
 function setQuiz(jsonObj){
-        console.log('現在のcurrentNumは' + currentNum);
+    console.log('現在のcurrentNumは' + currentNum);
     const Quiz = jsonObj.results[currentNum];
-        console.log(Quiz)//取得したクイズデータを確認
+    console.log(Quiz)//取得したクイズデータを確認
 
-    questionNumber.textContent = "問題" + QuestionNum
+    questionNumber.textContent = "問題" + (currentNum + 1)
 
     question.textContent = Quiz.question //APIからとってきた質問   
 
@@ -70,50 +69,54 @@ function setQuiz(jsonObj){
 
     //選択肢ボタンの描画
     shuffledAnswers.forEach((choice) => {
-        const li = document.createElement('li')
-        const btn = document.createElement('button')
-        btn.innerHTML = choice
-        li.appendChild(btn)
-        btnSection.appendChild(li)
-            //選択肢ボタンを押した時の処理
-            btn.addEventListener('click', () =>{
-                    //正誤チェック
-                    if(choice === Quiz.correct_answer){
-                        console.log('正解')
-                        score ++
-                    }else{
-                        console.log('間違い')
-                    }
-                    //選択肢ボタンを消す
-                    while (btnSection.firstChild){
-                                btnSection.removeChild(btnSection.firstChild)
-                            }
-                    while (catDifPlace.firstChild){
-                        catDifPlace.removeChild(catDifPlace.firstChild)
-                    }
- 
-                    //次のクイズへ
-                    currentNum ++
-                    QuestionNum ++
-                    if(currentNum === 10){
-                        questionNumber.textContent = "あなたの正答数は"　+ score + "です！！"
-                        question.textContent = "再チャレンジしたい場合は以下をクリック"
-                        const repeatBtn = document.createElement('button')
-                        repeatBtn.innerHTML = "ホームに戻る"
-                        btnSection.appendChild(repeatBtn)
-                            repeatBtn.addEventListener('click', () => {
-                                document.location.reload();
-                            })
-
-                    }else{
-                    setQuiz(jsonObj)
-                    }
-                    
-            })
+        createChoice(choice, jsonObj);
     })
 }
 
+//選択肢ボタンを描画する関数
+function createChoice(choice, jsonObj){
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+    btn.innerHTML = choice
+    li.appendChild(btn)
+    choiceBtnSection.appendChild(li)
+    //選択肢ボタンを押した時の処理
+    btn.addEventListener('click', () =>{
+        if(choice === jsonObj.results[currentNum].correct_answer){
+        //正誤チェック
+            console.log('正解')
+            score ++
+        }else{
+            console.log('間違い')
+        }
+        //選択肢ボタンを消す
+        while  (choiceBtnSection.firstChild){
+                 choiceBtnSection.removeChild (choiceBtnSection.firstChild)
+                }
+        while (catDifPlace.firstChild){
+            catDifPlace.removeChild(catDifPlace.firstChild)
+        }
+        //次のクイズへ
+        currentNum ++
+        if(currentNum === 10){
+            setAnswer();
+        }else{
+        setQuiz(jsonObj);
+        }
+    })
+}
 
+//結果を表示
+function setAnswer(){
+    questionNumber.textContent = "あなたの正答数は"　+ score + "です！！"
+    question.textContent = "再チャレンジしたい場合は以下をクリック"
+    const repeatBtn = document.createElement('button')
+    repeatBtn.innerHTML = "ホームに戻る"
+    choiceBtnSection.appendChild(repeatBtn)
+        repeatBtn.addEventListener('click', () => {
+            document.location.reload();
+        })
+}
 
 
 
